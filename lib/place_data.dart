000e7e581d338b4eff8f1edba4854b704814c321
Michael Ryan <michael_ryan_2000@yahoo.com>
2019-10-26 15:38:46 -0400

    Added Firebase Read

--- lib/place_data.dart
+++ lib/place_data.dart
@@ -1,17 +1,58 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Place {
+  String name;
   String type;
   DateTime expiration;
   LatLng location;
   List<Food> menu;
-  String metadata;
   int upvotes;
+  DocumentReference creator;
+  String id;
+  final DocumentReference reference;
+
+  Place.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['name'] != null),
+        assert(map['type'] != null),
+        assert(map['location'] != null),
+        assert(map['creator'] != null),
+        assert(map['upvotes'] != null),
+        assert(map['expiration'] != null),
+        name = map['name'],
+        type = map['type'],
+        location = LatLng(map['location'].latitude, map['location'].longitude),
+        creator = map['creator'],
+        upvotes = map['upvotes'],
+        expiration = new DateTime.fromMicrosecondsSinceEpoch(map['expiration'].seconds * 1000),
+        id = reference.documentID;
+
+
+  Place.fromSnapshot(DocumentSnapshot snapshot) :
+    this.fromMap(snapshot.data, reference: snapshot.reference);
+
 }
 
-class Food<T extends String, double> {
+class Food {
   String name;
   double rating;
-  List<T> cost;
+  String cost;
   List<String> dietaryRestrictions;
+  DocumentReference creator;
+  final DocumentReference reference;
+
+  Food.fromMap(Map<String, dynamic> map, {this.reference})
+    : assert(map['name'] != null),
+      assert(map['rating'] != null),
+      assert(map['cost'] != null),
+      assert(map['dietaryRestrictions'] != null),
+      assert(map['creator'] != null),
+      name = map['name'],
+      rating = map['rating'],
+      cost = map['cost'],
+      dietaryRestrictions = map['dietaryRestrictions'],
+      creator = map['creator'];
+
+  Food.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
 }
--- lib/social_view.dart
+++ lib/social_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import './place_data.dart';
 
 class SocialView extends StatefulWidget {
   @override
@@ -8,6 +11,44 @@ class SocialView extends StatefulWidget {
 class SocialViewState extends State<SocialView> {
   @override
   Widget build(BuildContext context) {
-    return Text('SocialView');
+    return _buildBody(context);
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('places').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        return _buildList(context, snapshot.data.documents);
+      },
+    );
+  }
+
+  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+    final place = Place.fromSnapshot(data);
+
+    return Padding(
+      key: ValueKey(place.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(place.name),
+          trailing: Text(place.id),
+          onTap: () => print(place.id),
+        ),
+      ),
+    );
   }
 }
