5da60a1ec646d204095622ead99478c549f3aa24
Michael Ryan <michael_ryan_2000@yahoo.com>
2019-10-26 11:46:33 -0400

    Added signin page

--- lib/settings_view.dart
+++ lib/settings_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import './register_page.dart';
+import './signin_page.dart';
 
 class SettingsView extends StatefulWidget {
   @override
@@ -7,9 +10,42 @@ class SettingsView extends StatefulWidget {
 }
 
 class SettingsViewState extends State<SettingsView> {
+  FirebaseUser user;
   @override
   Widget build(BuildContext context) {
-    return Text('SettingsView');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            child: RaisedButton(
+              child: const Text('Test registration'),
+              onPressed: () => _pushPage(context, RegisterPage()),
+            ),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+          Container(
+            child: RaisedButton(
+              child: const Text('Test SignIn/SignOut'),
+              onPressed: () => _pushPage(context, SignInPage()),
+            ),
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+          ),
+        ],
+      ),
+    );
+
+  }
+
+  void _pushPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (_) => page),
+    );
   }
 }
 
