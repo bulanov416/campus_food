b12b76037f52ff2d74114dd0e3551ad67eeeb431
Alex Bulanov <alexander@bulanov.com>
2019-10-26 01:46:23 -0400

    basic ui with menu navigation

---
+++ lib/frame.dart
@@ -0,0 +1,91 @@
+import 'package:campus_food/trending_view.dart';
+import 'package:flutter/material.dart';
+import 'map_view.dart';
+import 'social_view.dart';
+import 'settings_view.dart';
+
+class Frame extends StatefulWidget {
+  final String pageTitle;
+
+  Frame({Key key, this.pageTitle}) : super(key: key);
+
+  @override
+  _FrameState createState() => _FrameState();
+}
+
+class _FrameState extends State<Frame> {
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final _tabs = [
+      mapView(context),
+      trendingView(context),
+      socialView(context),
+      settingsView(context),
+    ];
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color(0xffbdd8ff),
+        centerTitle: true,
+        elevation: 0,
+        // Commented out is the button to the left of the title
+        /*leading: IconButton(
+          onPressed: () {},
+          iconSize: 21,
+          icon: Icon(Icons.map),
+        ),*/
+        title: Text('Campus Eats', textAlign: TextAlign.center, textDirection: TextDirection.ltr,),
+        // Commented out are the buttons to the right of the title
+        /*actions: <Widget>[
+          IconButton(
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+            iconSize: 21,
+            icon: Icon(Icons.zoom_in),
+          ),
+          IconButton(
+            padding: EdgeInsets.all(0),
+            onPressed: () {},
+            iconSize: 21,
+            icon: Icon(Icons.alarm),
+          ),
+        ],*/
+      ),
+      body: _tabs[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            title: Text('Map'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            title: Text('Trending'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: Text('Social'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          )
+        ],
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+}
