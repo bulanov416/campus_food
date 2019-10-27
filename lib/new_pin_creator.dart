36fcf8f1215f04f725978643da9155866da998e6
Michael Ryan <michael_ryan_2000@yahoo.com>
2019-10-27 04:27:17 -0400

    Allow user created markers

--- lib/new_pin_creator.dart
+++ lib/new_pin_creator.dart
@@ -1,8 +1,9 @@
 import 'package:campus_food/map_view.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'frame.dart';
+import './auth.dart';
 import 'place_data.dart';
 
 class NewPinCreator extends StatefulWidget {
@@ -78,14 +79,45 @@ class NewPinCreatorState extends State<NewPinCreator> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.check),
-        onPressed: () {
-          // UPLOAD newPlace OBJECT TO DATABASE
-          // DONT FORGET TO TAKE DATA FROM location AND UPLOAD TO DATABASE WITH OBJECT
-          Navigator.pop(context);
-        },
-      ),
+      floatingActionButton: _uploadButton(context),
+    );
+  }
+
+  Widget _uploadButton(BuildContext context) {
+    DateTime now = DateTime.now();
+    DateTime expire = DateTime(now.year, now.month, now.day+1, 0, 0, 0);
+    return StreamBuilder<DocumentSnapshot>(
+      stream: Firestore.instance.collection('places').document().snapshots(),
+      builder: (context, snapshot) {
+        return FloatingActionButton(
+          child: Icon(Icons.check),
+
+          onPressed: () {
+            if (Auth.user != null) {
+              // UPLOAD newPlace OBJECT TO DATABASE
+              // DONT FORGET TO TAKE DATA FROM location AND UPLOAD TO DATABASE WITH OBJECT
+              snapshot.data.reference.setData({
+                "name": name,
+                "creator": Firestore.instance.collection('users').document(
+                    Auth.user.uid),
+                "downvoters": [],
+                "upvoters": [],
+                "timeToExpire": expire.millisecondsSinceEpoch,
+                "expiration": expire,
+                "type": "Free Food",
+                "upvotes": 0,
+                "location": new GeoPoint(location.latitude, location.longitude),
+                "createdAt": now
+              });
+              Navigator.pop(context);
+            } else {
+              Auth.refreshFirebaseUser();
+              Auth.SignInAlert(
+                  context, "You need to be signed in to add pins to the map.");
+            }
+          },
+        );
+      }
     );
   }
 }
